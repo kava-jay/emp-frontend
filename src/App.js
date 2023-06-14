493442c649b57fd516b3f60b440615176162d274
kava-jay <122340417+kava-jay@users.noreply.github.com>
2023-06-14 09:08:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   const [employeeList, setEmployeeList] = useState([]);
 //  const baseUrl = "http://localhost:3001"
-  const baseUrl = "http://20.204.152.7:80/"
+  const baseUrl = "http://20.204.152.7:80"
 
   const addEmployee = () => {
     Axios.post(`${baseUrl}/create`, {
